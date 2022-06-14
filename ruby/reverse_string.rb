f8da68ed6e9dac721a68ad2dc66b69d46594f15e
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-14 15:34:22 -0400

    second implementation - for loop

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -12,16 +12,37 @@ def reverse_string(str)
   return_arr.join
 end
 
+def reverse_string2(str)
+  length = str.length
+
+  if length == 0 then return "" end
+
+  arr = []
+
+  for i in (length-1).downto(0)
+    arr << str[i]
+  end
+
+  arr.join
+end
+
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
-  puts "=>", reverse_string('hi')
+  puts "=>", reverse_string2('hi')
 
   puts
 
   puts "Expecting: 'ybabtac'"
-  puts "=>", reverse_string('catbaby')
+  puts "=>", reverse_string2('catbaby')
 
   # Don't forget to add your own!
+  puts
+  puts "Expecting: ''"
+  puts "=>", reverse_string2('')
+
+  puts
+  puts "Expecting: 'b'"
+  puts "=>", reverse_string2('b')
 end
 
 # Please add your pseudocode to this file
@@ -43,3 +64,11 @@ end
 # while len of original array > 0
     # .pop each element from the old array to the new array
 # return new array.join (array joined into a string)
+
+# Method 3
+# save the length of the string into a counter variable
+# create a new empty array
+# have a for loop that decrements the counter variable
+    # read the indexed character of the string and shovel it into the array
+# return array.join
+# - this might be both more time and space efficient than previous method
