66be213c0fafc2da1c40f62285beba0e12e5581a
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-14 15:15:43 -0400

    first method - pop

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,15 @@
 def reverse_string(str)
   # type your code in here
+  if str.length == 0 then return "" end
+
+  arr = str.split("")
+  return_arr = []
+
+  while arr.count > 0
+    return_arr << arr.pop
+  end
+
+  return_arr.join
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -16,3 +26,20 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# Problem
+# Given a string, return a string that is its reverse ('abc' -> 'cba')
+
+# Pseudocode
+
+# Method 1
+# Use .reverse LOL
+
+# Method 2
+# check the length of the string
+    # if == 0, return ""
+# turn string into an array with .split
+# create a new empty array
+# while len of original array > 0
+    # .pop each element from the old array to the new array
+# return new array.join (array joined into a string)
